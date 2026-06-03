2e6c82741fdd794d98453dd8160db8bbf618cb86
juhairulislam <juhairulislam2018@gmail.com>
2026-06-03 10:50:01 +0600

    create register page

---
+++ src/app/register/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RegisterPage = () => {
+    return (
+        <div>
+            This is register page re bhai
+        </div>
+    );
+};
+
+export default RegisterPage;
