0c0a77e328497753bf4af2021af77922fb32b78c
juhairulislam <juhairulislam2018@gmail.com>
2026-06-04 17:10:33 +0600

    create recruiter page

---
+++ src/app/dashboard/recruiter/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const RecruiterPage = () => {
+    return (
+        <div>
+            <h1>This is recruiter</h1>
+            
+        </div>
+    );
+};
+
+export default RecruiterPage   ;
