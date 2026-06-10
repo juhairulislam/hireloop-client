edfcf7328dee0c0dfbf6d54733cd8aac42b85a9b
juhairulislam <juhairulislam2018@gmail.com>
2026-06-10 14:17:10 +0600

    change the path of sign in in sessoin file

--- src/lib/core/session.js
+++ src/lib/core/session.js
@@ -13,7 +13,7 @@ export const getUserSession = async () => {
 export const requireRole = async(role) =>{
     const user = await getUserSession()
     if(!user){
-        redirect('/auth/signin')
+        redirect('/signin')
     }
     if(user?.role !== role){
         redirect('/unauthorized')
