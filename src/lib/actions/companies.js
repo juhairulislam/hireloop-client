195286fc62d80c5508b0594e100f915376f54806
juhairulislam <juhairulislam2018@gmail.com>
2026-06-11 14:24:20 +0600

    remove comment from companies js file

--- src/lib/actions/companies.js
+++ src/lib/actions/companies.js
@@ -16,16 +16,3 @@ export const updateCompany = async (id, data) => {
 
 
 
-// const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
-
-// export const createCompany = async (newCompanyData) => {
-//     const res = await fetch(`${baseUrl}/api/companies`, {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify(newCompanyData),
-//     });
-
-//     return res.json();
-// }
