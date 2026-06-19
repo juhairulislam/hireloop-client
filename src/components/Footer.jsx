56af21c32810028c0c9180c622ded38b9b0021a3
juhairulislam <juhairulislam2018@gmail.com>
2026-06-19 12:14:05 +0600

    remove comment from footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -85,7 +85,6 @@ export default function Footer() {
 
         <div className="pt-8 border-t border-zinc-900/50 flex flex-col sm:flex-row items-center justify-between gap-6">
           
-          {/* Social Media Icons */}
           <div className="flex items-center gap-3">
             <a
               href="#"
